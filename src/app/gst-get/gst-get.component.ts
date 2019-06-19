d5fe1bbc3d53ada55a60f55f776e0f84f923a383
Nesrine Mouhoubi <n.mouhoubi@outlook.com>
2019-06-19 18:26:32 +0100

    add files

---
+++ src/app/gst-get/gst-get.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import Business from '../Business';
+import { BusinessService } from '../business.service';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-gst-get',
+  templateUrl: './gst-get.component.html',
+  styleUrls: ['./gst-get.component.css']
+})
+export class GstGetComponent implements OnInit {
+
+  businesses: Business[];
+
+  constructor(
+    private bs: BusinessService,
+    private router: Router,
+  ) { }
+
+  ngOnInit() {
+    this.bs
+      .getBusinesses()
+      .subscribe((data: Business[]) => {
+        this.businesses = data;
+    });
+  }
+
+  deleteBusiness(id) {
+    this.bs.deleteBusiness(id).subscribe((id) => console.log(`${id} is deleted`));
+  }
+}
